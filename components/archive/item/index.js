db1124dc3318d1c66bb16cf389f0b696540316d9
mathiasdag <mathias@asso.gd>
2021-08-28 16:25:29 +0200

    ...

--- components/archive/item/index.js
+++ components/archive/item/index.js
@@ -1,4 +1,5 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
 import Asset from './asset'
 import styles from './assets.module.css'
 import Header from '../../header/index'
@@ -18,12 +19,17 @@ export default function Item({ data, index, archive }) {
     slug
   } = data
 
+  const options = {
+    renderNode: {
+      [BLOCKS.HEADING_1]: (node, children) => <h1>{children}</h1>,
+    }
+  }
+
   return (
     <article className={styles.container}>
     <section className={styles.introduction + " block soft-breaks"}>
       <Header index={index} />
-        <h1>–{title}–</h1>
-        {documentToReactComponents(textField)}
+      {documentToReactComponents(textField, options)}
     </section>
       {assets.map((asset, i) => (
         <Asset data={asset} key={i} i={i} />
