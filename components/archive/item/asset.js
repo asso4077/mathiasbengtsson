8702efd06dc22de6c9e788e33273804a13bc9c76
mathiasdag <mathias@asso.gd>
2021-12-07 11:08:56 +0100

    error handling

--- components/archive/item/asset.js
+++ components/archive/item/asset.js
@@ -1,5 +1,7 @@
 import styles from './assets.module.css'
 import { Img } from '../../../lib/image'
+import Image from 'next/image'
+import { contentful } from '../../../lib/loaders'
 import Video from '../../video/'
 import slugify from 'react-slugify'
 import { use100vh } from 'react-div-100vh'
@@ -20,17 +22,21 @@ export default function Asset({ data, i }) {
   const position = rightAligned === true ? " right-aligned " : rightAligned === false ? " left-aligned " : " centered "
   const isFullscreen = fullscreen ? "fullscreen" : "margins"
   return (
-    <figure className={`${styles.asset} asset-grid ${slugify(layout)}`} style={{ height: height }}>
+    <figure className={`${styles.asset} asset-grid ${slugify(layout ?? 'left-aligned')}`} style={{ height: height }}>
       {
-        asset.fields.file.contentType.includes('image')
-        ? <Img
-            src={asset.fields.file.url}
-            width={asset.fields.file.details.image.width}
-            height={asset.fields.file.details.image.height}
-            alt={asset.fields.description ?? "No description is available"}
-            />
+        asset?.fields?.file?.contentType.includes('image')
+        ? <div className={`image-container`}>
+            <Image
+              src={asset?.fields?.file?.url}
+              width={asset?.fields?.file?.details.image.width}
+              height={asset.fields.file.details.image.height}
+              loader={contentful}
+              layout={'responsive'}
+              alt={asset.fields.description ?? "No description is available"}
+              />
+          </div>
         : <Video
-            src={asset.fields.file.url}
+            src={asset?.fields?.file?.url}
             controls={!slugify(layout).includes('fullscreen')}
             muted={slugify(layout).includes('fullscreen')}
             loop={slugify(layout).includes('fullscreen')}
